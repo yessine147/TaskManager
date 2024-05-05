f0ebb9b6255a18eafdc0865895fc7243768f1fd5
yessine-ben-jemaa <yessine.benjemaa47@gmail.com>
2024-05-06 00:00:08 +0100

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import './App.css';
-import { Navigate, Route, Routes, BrowserRouter as Router } from 'react-router-dom';
-import TaskList from './components/TaskList';
+import {
+  Navigate,
+  Route,
+  Routes,
+  BrowserRouter as Router,
+} from "react-router-dom";
+import TaskList from "./components/TaskList";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <Router>
-    <Routes>
-    <Route path="/tasks" element={<TaskList />} />
-    <Route path='/*' element={<Navigate to='/tasks' replace />} />
-  </Routes>
-  </Router>
+      <Navbar />
+      <div className="container mt-3">
+        <Routes>
+          <Route path="/tasks" element={<TaskList />} />
+          <Route path="/*" element={<Navigate to="/tasks" replace />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 const queryClient = new QueryClient();
@@ -17,5 +16,3 @@ root.render(
     </React.StrictMode>
   </QueryClientProvider>
 );
-
-reportWebVitals();
