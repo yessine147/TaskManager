2b1d80299639165d6b64d9f1a5cb14b49b9ad58e
yessine-ben-jemaa <yessine.benjemaa47@gmail.com>
2024-05-06 01:39:18 +0100

    add SnackbarProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { SnackbarProvider } from "notistack";
 
 const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <QueryClientProvider client={queryClient}>
-    <React.StrictMode>
+    <SnackbarProvider maxSnack={3}>
       <App />
-    </React.StrictMode>
+    </SnackbarProvider>
   </QueryClientProvider>
 );
